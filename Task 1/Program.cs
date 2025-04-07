c61ccb5b4d96a2e8efa4b04ae2414faa6bc0e6c7
MohamedRizwan2214 <MohamedRizwan2214@gmail.com>
2025-04-07 10:07:46 +0530

    Task 1

---
+++ Task 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static int call(int n)
+    {
+        if(n==0||n==1)
+        return 1;
+        else
+        return n*call(n-1);
+    }
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        // int n=5;
+        int fact=1;
+        
+        for(int i=1;i<=n;i++)
+        {
+            fact*=i;
+        }
+        // Console.WriteLine(Program.call(n));
+        Console.WriteLine(fact);
+        Console.Write(Program.call(n));
+    }
+}
