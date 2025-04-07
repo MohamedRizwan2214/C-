82ea57e9f2e967186e4ffc3c0c0f79f24c97e2b5
MohamedRizwan2214 <MohamedRizwan2214@gmail.com>
2025-04-07 11:13:17 +0530

    Task 2

---
+++ Task 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Person{
+    string name;
+    int age;
+    public Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+    public void Inroduce()
+    {
+        Console.WriteLine("Hello, my name is " + name+ " and I am " + age + " years old.");
+    }
+}
+class Program{
+    static void Main(string[] args)
+    {
+        int x=5;
+        Console.WriteLine("Hello World!"+ x);
+        Person person1 = new Person("John", 30);
+        Person person2 = new Person("Jane", 25);
+        person1.Inroduce();
+        person2.Inroduce();
+    }
+}
