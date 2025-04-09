094e4e91ca17351fdf519f0af4eaeb194453cb4f
MohamedRizwan2214 <MohamedRizwan2214@gmail.com>
2025-04-09 12:40:57 +0530

    Task 7

---
+++ Task 7/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+class Program
+{
+    static async Task<string> callApi(string apiName, int delay)
+    {
+        try{
+            Console.WriteLine($"Calling {apiName}...");
+            await Task.Delay(delay); 
+            return $"{apiName} responded after {delay} ms.";
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+            return $"Error calling {apiName}";
+        }
+    }
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Starting Data Fetch");
+        try
+        {
+            List<Task<string>> tasks=new List<Task<string>>();
+            tasks.Add(callApi("API1",1000));
+            tasks.Add(callApi("API2",1500));
+            tasks.Add(callApi("API3",800));
+
+            string[] results = await Task.WhenAll(tasks);
+            Console.WriteLine("All API calls completed successfully.");
+            foreach (var result in results)
+            {
+                Console.WriteLine(result);
+            }
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+        finally
+        {
+            Console.WriteLine("Data Fetch Completed");
+        }
+    }
+
+}
